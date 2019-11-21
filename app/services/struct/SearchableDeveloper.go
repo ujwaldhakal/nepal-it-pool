a2787cf0e2bf47ef74d6e5ab676ffc40878734c0
ujwal dhakal <kevin.ujwal@gmail.com>
2019-11-21 22:20:50 +0545

    Updated migration for new data and added some search fields

--- app/services/struct/SearchableDeveloper.go
+++ app/services/struct/SearchableDeveloper.go
@@ -3,8 +3,11 @@ package SearchableDeveloper
 type DeveloperSearchableFields struct {
   Name string `form:"name" json:"name,omitempty"`
   Designation string `form:"designation" json:"designation,omitempty"`
-  LookingForJob string `form:"actively_job_searching" json:"actively_job_searching,omitempty"`
+  LookingForJob bool `form:"actively_job_searching" json:"actively_job_searching,omitempty"`
   MaxExperience string `form:"max_exp" json:"max_exp,omitempty"`
   MinExperience string `form:"min_exp" json:"min_exp,omitempty"`
   Skills string `form:"skills" json:"skills,omitempty"`
+  Intern bool `form:"is_intern" json:"is_intern,omitempty"`
+  Address string `form:"address" json:"address,omitempty"`
+  State string   `form:"state" json:"state,omitempty"`
 }
